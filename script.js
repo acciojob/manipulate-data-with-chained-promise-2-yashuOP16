cb09cbd7c804f0aa3800fb1a5954735cf7faebdb
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-05-13 14:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+
+const promise = new promise ( (resorve , reject) => {
+	setTimeout (() => {
+		resorve([1,2,3,4])
+	}, 1000)
+})
+
+promise.then((evenData) => {
+	const multipleNumber = evenData.map((num) =>{
+			return num * 2
+	});
+
+	setTimeout (() => {
+		output.innerText = multipleNumber;
+	}, 3000)
+	
+	console.log(evenData)
+})
